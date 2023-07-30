ef15f55e1cb10f75d727d2a4ad3643134fc9c627
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2023-07-30 21:47:31 +0200

    Update Sliders.js

--- components/Sliders.js
+++ components/Sliders.js
@@ -1,19 +1,17 @@
 // components/Slider.js
 import Image from "next/image";
-import React from "react";
-import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper/core";
+import SwiperCore from "swiper/core";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import "swiper/css/bundle";
 
-SwiperCore.use([Navigation, Pagination, Autoplay]);
+SwiperCore.use([Autoplay]);
 
 const Slider = ({ slides }) => {
   return (
     <Swiper
       spaceBetween={92}
       slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
       autoplay={{ delay: 2500, disableOnInteraction: false }}
       loop={true}
     >
