0037e9095f3c31c0ce9f459e6a58224d5b43d1c1
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2023-07-30 20:29:49 +0200

    update

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,16 @@
 import styles from "../styles/Home.module.scss";
 
 export default function Footer() {
-  return <div className={styles.footer_container}>Footer</div>;
+  return (
+    <>
+      <div className={styles.footer_container}>
+        <ul>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+          <li>FAQS</li>
+          <li>SHIPPING</li>
+        </ul>
+      </div>
+    </>
+  );
 }
